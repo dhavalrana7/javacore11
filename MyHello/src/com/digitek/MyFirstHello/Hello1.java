3b70506c00516a34d8824e56a114f71d5e4f1479
dhavalrana7 <dhaval_rana@yahoo.com>
2016-01-02 20:56:40 -0600

    with scanner
    with scanner

--- MyHello/src/com/digitek/MyFirstHello/Hello1.java
+++ MyHello/src/com/digitek/MyFirstHello/Hello1.java
@@ -1,9 +1,20 @@
 package com.digitek.MyFirstHello;
 
+import java.util.Scanner;
+
 public class Hello1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("please enter some number :");
+		Scanner scan = new Scanner(System.in);
+		 	double my_number=scan.nextDouble();
+		 			
+		System.out.println("Entered Number is : "+my_number); 
+			if (my_number>=0) {
+				System.out.println("Entered Number is Positive.");}
+				else {System.out.println("Enterd number is Nagative.");
+			}
 
 	}
 
